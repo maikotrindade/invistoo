66c70eae1f2d4970b3bd4c950f9c4fd04dcd99e3
Maiko Trindade <maikocharles@gmail.com>
2016-01-26 22:51:14 -0200

    create package 'ui' and 'components', refactor Dashboard Activity and add Circle Image View

--- app/src/main/java/com/jumbomob/invistoo/ui/DashboardActivity.java
+++ app/src/main/java/com/jumbomob/invistoo/ui/DashboardActivity.java
@@ -1,4 +1,4 @@
-package com.jumbomob.invistoo;
+package com.jumbomob.invistoo.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.jumbomob.invistoo.R;
 
 public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
