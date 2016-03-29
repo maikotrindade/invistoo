4fd3632deb9082511ae096c97df6fd842a3a9942
Maiko Trindade <maikocharles@gmail.com>
2016-03-28 22:40:20 -0300

    add Question entity

---
+++ app/src/main/java/com/jumbomob/invistoo/model/entity/Question.java
@@ -0,0 +1,59 @@
+package com.jumbomob.invistoo.model.entity;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author maiko.trindade
+ * @since 28/03/2016
+ */
+public class Question {
+
+    @SerializedName("_id")
+    @Expose
+    private String id;
+
+    @SerializedName("question")
+    @Expose
+    private String question;
+
+    @SerializedName("answer")
+    @Expose
+    private String answer;
+
+    @SerializedName("group")
+    @Expose
+    private String group;
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
